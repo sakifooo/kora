e9db4849203f51ab58355f2ef8a425efb4f607ef
sakifooo <saki.sasin@gmail.com>
2026-06-12 00:33:26 +0100

    Update match time for Canada vs Bosnia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import {
 export default function Home() {
   const matches = [
     { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 0, scoreB: 0, time: "20:00 ", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
-    { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 4, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
+    { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 4, time: "20:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
 
   ];
 
