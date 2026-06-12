d00bb5698ca003cf255fbb6036b42678c0bf26bf
sakifooo <saki.sasin@gmail.com>
2026-06-12 22:06:21 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,8 @@ import {
 
 export default function Home() {
   const matches = [
-    { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 1, time: "20:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
-    { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 2, scoreB: 1, time: "20:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
-
+    { teamA: "الولايات المتحدة ", teamB: "البراغواي ", scoreA: 0, scoreB: 0, time: "2:00 ", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/usa-vs-paraguay" },
+    { teamA: "كندا", teamB: "البوسنة", scoreA: 1, scoreB: 1, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
   ];
 
   const seoKeywords = [
