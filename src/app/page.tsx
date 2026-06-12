54816c86c9b5b120e0f21584f8d6cd1e0e81502d
sakifooo <saki.sasin@gmail.com>
2026-06-12 20:36:04 +0100

    Update match score and status for Canada vs Bosnia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function Home() {
   const matches = [
-    { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 4, time: "20:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
+    { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 1, time: "20:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
     { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 2, scoreB: 1, time: "20:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
 
   ];
