93af942ae2f27717fd020ad3ad67fbdb0001dd9b
sakifooo <saki.sasin@gmail.com>
2026-06-14 23:28:09 +0100

    Update iframe mapping for live match links

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -24,8 +24,8 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
    */
   const iframeMapping: Record<string, string> = {
     // المباراة الأولى (وادي دجلة ضد زد)
-    "germany-vs-corasaw": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
-    "holand-vs-japan": "https://8.kooralive360.com/albaplayer/bein-sports-hd-2/?serv=1",
+    "cotedivoire-vs-ecwador": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "swid-vs-tunis": "https://8.kooralive360.com/albaplayer/bein-sports-hd-2/?serv=1",
     
   };
 
