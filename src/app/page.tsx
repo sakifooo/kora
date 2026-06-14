373a8686aac034a1800917cc72a9a26009d59ea1
sakifooo <saki.sasin@gmail.com>
2026-06-14 20:41:46 +0100

    Update match details for Germany and Netherlands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,9 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "المانيا ", teamB: "كوراساو ", scoreA: 5, iscoreB: 1, time: "18:00 ", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/germany-vs-corasaw" },
-    { teamA: "هولندا", teamB: "اليابان", scoreA: 0, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/holand-vs-japan" },
+
+    { teamA: "هولندا", teamB: "اليابان", scoreA: 0, scoreB: 0, time: "21:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/holand-vs-japan" },
+    { teamA: "المانيا ", teamB: "كوراساو ", scoreA: 7, iscoreB: 1, time: "18:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/germany-vs-corasaw" },
     { teamA: "استراليا", teamB: "تركيا", scoreA: 2, scoreB: 0, time: "05:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/turc-vs-australia" },
     { teamA: "سكتلندا", teamB: "هايتي", scoreA: 1, scoreB: 0, time: "02:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: " غير معروف", league: "كأس العالم", href: "/live/scotland-vs-haiti" },
     { teamA: "المغرب ", teamB: "البرازيل ", scoreA: 1, scoreB: 1, time: "2:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-brazil" },
