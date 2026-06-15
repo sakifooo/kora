425dd2b9e1fedd344891002885776320f5869290
sakifooo <saki.sasin@gmail.com>
2026-06-15 13:08:22 +0100

    Update image URL path in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     type: 'website',
       images: [
     {
-      url: 'og-image.jpg',
+      url: '/og-image.jpg',
       width: 1200,
       height: 630,
       alt: 'Koora Live',
