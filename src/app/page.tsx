0439a83570f1e3a004ceb3c66f90f99566f3c976
sakifooo <saki.sasin@gmail.com>
2026-06-17 00:35:36 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "فرنسا", teamB: "السنغال", scoreA: 0, scoreB: 0, time: "20:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/france-vs-senigal" },
-    { teamA: "العراق", teamB: "النرويج", scoreA: 1, scoreB: 1, time: "23:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-iraq" },
+    { teamA: "فرنسا", teamB: "السنغال", scoreA: 3, scoreB: 1, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/france-vs-senigal" },
+    { teamA: "العراق", teamB: "النرويج", scoreA: 1, scoreB: 2, time: "23:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-iraq" },
     { teamA: "ايران ", teamB: "نيوزيلاندا  ", scoreA: 2, scoreB: 2, time: "17:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/spain-vs-capvert" },
 
   ];
