b4142200203a894ea243c6a4e5b3f75de5bb3dbb
sakifooo <saki.sasin@gmail.com>
2026-06-16 12:15:48 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "أوروجواي", teamB: "السعودية", scoreA: 0, scoreB: 0, time: "23:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/saudi-vs-uruguay" },
-    { teamA: "بلجيكا", teamB: "مصر", scoreA: 1, scoreB: 1, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/belgic-vs-egypt" },
-    { teamA: "إسبانيا ", teamB: "الرأس الأخضر ", scoreA: 0, scoreB: 0, time: "17:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/spain-vs-capvert" },
+    { teamA: "فرنسا", teamB: "السنغال", scoreA: 0, scoreB: 0, time: "20:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/france-vs-senigal" },
+    { teamA: "العراق", teamB: "النرويج", scoreA: 1, scoreB: 1, time: "23:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-iraq" },
+    { teamA: "ايران ", teamB: "نيوزيلاندا  ", scoreA: 2, scoreB: 2, time: "17:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/spain-vs-capvert" },
 
   ];
   const seoKeywords = [
