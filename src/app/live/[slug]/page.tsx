75f6b5ec664e3c782c9707743e288857ac4aa2f0
sakifooo <saki.sasin@gmail.com>
2026-06-16 12:17:05 +0100

    Update page.tsx

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -24,8 +24,8 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
    */
   const iframeMapping: Record<string, string> = {
     // المباراة الأولى (وادي دجلة ضد زد)
-   "spain-vs-capvert": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
-    "belgic-vs-egypt": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "france-vs-senigal": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "norway-vs-iraq": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
     "saudi-vs-uruguay": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
 
   };
