60889eb598da24ff86110e5db4dfe92a4627fb66
sakifooo <saki.sasin@gmail.com>
2026-06-17 20:40:29 +0100

    Update match status from 'live' to 'finished'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "البرتغال", teamB: "الكونغو", scoreA: 0, scoreB: 0, time: "18:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/portugal-vs-congo" },
+    { teamA: "البرتغال", teamB: "الكونغو", scoreA: 0, scoreB: 0, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/portugal-vs-congo" },
     { teamA: "انكلترا", teamB: "كرواتيا", scoreA: 0, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/england-vs-croitia" },
     { teamA: "الجزائر ", teamB: "الارجنتين", scoreA: 0, scoreB: 3, time: "02:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/alger-vs-argentina" },
     { teamA: "النمسا", teamB: "الاردن", scoreA: 3, scoreB: 1, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-iraq" },
