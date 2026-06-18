385985787f733d4dbc90f055b890c56399666755
sakifooo <saki.sasin@gmail.com>
2026-06-18 19:41:47 +0100

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://kora-shot.vercel.app/sitemap.xml',
+    sitemap: 'https:/kooratv.site/sitemap.xml',
   }
 }
