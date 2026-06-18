4e78b389acd91a1d95ccf705982ed3db72da834f
sakifooo <saki.sasin@gmail.com>
2026-06-18 19:45:25 +0100

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https:/kooratv.site/sitemap.xml',
+    sitemap: 'https://kooratv.site/sitemap.xml',
   }
 }
