8b85efbbc82d11a404f17664c531194c30777432
sakifooo <saki.sasin@gmail.com>
2026-06-18 19:50:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   
   const matches = [
   
-    { teamA: "تشيكيا", teamB: "جنوب افريقيا", scoreA: 0, scoreB: 0, time: "17:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/check-vs-southafrica" },
-    { teamA: "سويسرا", teamB: "البوسنة", scoreA: 0, scoreB: 0, time: "20:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/swis-vs-bosnia" },
+    { teamA: "سويسرا", teamB: "البوسنة", scoreA: 0, scoreB: 0, time: "20:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/swis-vs-bosnia" },
+    { teamA: "تشيكيا", teamB: "جنوب افريقيا", scoreA: 1, scoreB: 1, time: "17:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/check-vs-southafrica" },
     { teamA: "كندا", teamB: "قطر", scoreA: 0, scoreB: 0, time: "23:00", status: "upcoming", channel: "beIN SPORTS HD1", commentator: "غير معروف", league: "كأس العالم", href: "/live/qatar-vs-canada" },
     { teamA: "كلومبيا", teamB: "ازبكشتان", scoreA: 3, scoreB: 1, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/portugal-vs-congo" },
     { teamA: "بنما ", teamB: "غانا", scoreA: 0, scoreB: 1, time: "02:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/panama-vs-gana" },
