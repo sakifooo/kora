802ece85f1a27571c09fe86b9cab69776275ab40
sakifooo <saki.sasin@gmail.com>
2026-06-19 19:58:37 +0100

    Change match status from 'upcoming' to 'live'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "الولايات المتحدة", teamB: "استراليا", scoreA: 0, scoreB: 0, time: "20:00", status: "upcoming", channel: "beIN SPORTS HD1", commentator: "غير معروف", league: "كأس العالم", href: "/live/usa-vs-australia" },
+    { teamA: "الولايات المتحدة", teamB: "استراليا", scoreA: 0, scoreB: 0, time: "20:00", status: "live", channel: "beIN SPORTS HD1", commentator: "غير معروف", league: "كأس العالم", href: "/live/usa-vs-australia" },
     { teamA: "المغرب", teamB: "سكتلندا", scoreA: 0, scoreB: 0, time: "23:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-scotland" },
     { teamA: "البرازيل", teamB: " هايتي", scoreA: 0, scoreB:0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/brazil-vs-haiti" },
     { teamA: "تركيا", teamB: " البراغواي", scoreA: 0, scoreB:0, time: "04:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/turc-vs-paraguay" },
