f4b5d9239d95d32afc1617a4114613175c308bd4
sakifooo <saki.sasin@gmail.com>
2026-06-20 01:55:57 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "المغرب", teamB: "سكتلندا", scoreA: 0, scoreB: 0, time: "23:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-scotland" },
+    { teamA: "المغرب", teamB: "سكتلندا", scoreA: 1, scoreB: 0, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-scotland" },
     { teamA: "الولايات المتحدة", teamB: "استراليا", scoreA: 2, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS HD1", commentator: "غير معروف", league: "كأس العالم", href: "/live/usa-vs-australia" },
-    { teamA: "البرازيل", teamB: " هايتي", scoreA: 0, scoreB:0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/brazil-vs-haiti" },
-    { teamA: "تركيا", teamB: " البراغواي", scoreA: 0, scoreB:0, time: "04:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/turc-vs-paraguay" },
+    { teamA: "البرازيل", teamB: " هايتي", scoreA: 0, scoreB:0, time: "01:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/brazil-vs-haiti" },
+    { teamA: "تركيا", teamB: " البراغواي", scoreA: 0, scoreB:0, time: "04:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/turc-vs-paraguay" },
     { teamA: "المكسيك", teamB: "كوريا الجنوبية", scoreA: 1, scoreB: 0, time: "02:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/mexico-vs-korea" },
 
   ];
