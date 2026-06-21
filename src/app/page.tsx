1df11b3babf076f370c9a98242f2d19168904d8b
sakifooo <saki.sasin@gmail.com>
2026-06-21 12:30:28 +0100

    Fix score formatting for Tunisia vs Japan match

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     { teamA: "أوروغواي", teamB: "الرأس الأخضر", scoreA: 0, scoreB: 0, time: "23:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/urugway-vs-capverdi" },
     { teamA: "مصر", teamB: "نيوزيلندا", scoreA: 0, mscoreB: 0, time: "02:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-newzeland" },
     { teamA: "الاكوادور", teamB: "كوراساو", scoreA: 0, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-corasaw" },
-    { teamA: "تونس", teamB: "اليابان", scoreA: 0, mscoreB: 4, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-japon" },
+    { teamA: "تونس", teamB: "اليابان", scoreA: 0, scoreB: 4, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-japon" },
 
   ];
   const seoKeywords = [
