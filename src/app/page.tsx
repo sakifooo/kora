f6cb5ca529cc9a038596a885462e56ea30b338b3
sakifooo <saki.sasin@gmail.com>
2026-06-22 12:05:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,11 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "إسبانيا", teamB: "السعودية  ", scoreA: 4, scoreB: 0, time: "17:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/spain-vs-saudi" },
-    { teamA: "بلجيكا", teamB: "إيران", scoreA: 0, mscoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/belgique-vs-iran" },
-    { teamA: "أوروغواي", teamB: "الرأس الأخضر", scoreA: 2, scoreB: 2, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/urugway-vs-capverdi" },
-    { teamA: "مصر", teamB: "نيوزيلندا", scoreA: 0, mscoreB: 0, time: "02:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-newzeland" },
-    { teamA: "الاكوادور", teamB: "كوراساو", scoreA: 0, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-corasaw" },
-    { teamA: "تونس", teamB: "اليابان", scoreA: 0, scoreB: 4, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-japon" },
-
+    { teamA: "الأرجنتين", teamB: "النمسا", scoreA: 0, scoreB: 0, time: "18:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/argentin-vs-austria" },
+    { teamA: "فرنسا", teamB: "العراق", scoreA: 0, mscoreB: 0, time: "22:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/france-vs-iraq" },
+    { teamA: "النرويج", teamB: "السنغال ", scoreA: 0, scoreB: 0, time: "23:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-senigal" },
+    { teamA: "مصر", teamB: "نيوزيلندا", scoreA: 3, mscoreB: 1, time: "02:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-newzeland" },
+     
   ];
   const seoKeywords = [
     "كورة لايف", "koora live", "مباريات اليوم بث مباشر", "يلا شوت", "yalla shoot", 
