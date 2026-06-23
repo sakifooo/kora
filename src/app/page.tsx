c5c21e748b5bfbac33da7fac71314fe1f4eba4c9
sakifooo <saki.sasin@gmail.com>
2026-06-23 17:55:16 +0100

    Change match status to 'live' for Portugal vs Uzbekistan
    Updated match status for the Portugal vs Uzbekistan game to 'live'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
 export default function Home() {
   
   const matches = [
-    { teamA: "البرتغال", teamB: "ازبكستان", scoreA: 0, scoreB: 0, time: "18:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/1-vs-2" },
+    { teamA: "البرتغال", teamB: "ازبكستان", scoreA: 0, scoreB: 0, time: "18:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/1-vs-2" },
     { teamA: "انكلترا", teamB: "غانا", scoreA: 3, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/1-vs-2" },
     { teamA: "النرويج", teamB: "السنغال ", scoreA: 3, scoreB: 2, time: "01:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-senigal" },
     { teamA: "الجزائر", teamB: "الاردن", scoreA: 2, scoreB: 1, time: "02:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-newzeland" },     
